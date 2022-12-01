b39063c0f7885fe11b724244bb48319a770296bc
rob.burley <rob.burley@itech.media>
2022-12-01 10:28:06 +0000

    Add return type

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Repositories\PlanetDistanceRepository\PlanetDistanceRepositoryInterface;
 use App\Services\PlanetDistanceConversion;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class HelloWorldController extends Controller
@@ -15,7 +16,7 @@ class HelloWorldController extends Controller
     {
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         $distance = $this->planetDistanceRepository->calculate($request->get('from'), $request->get('to'));
 
