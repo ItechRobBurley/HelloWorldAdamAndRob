640a9fe4a0f90e5f3d0f957def91fa2a0f92027b
iTechAdamHembrough <adam.hembrough@itech.media>
2022-12-01 10:48:53 +0000

    Distance calculation

--- app/Repositories/PlanetDistanceRepository/FromConfig/PlanetDistanceFromConfigRepository.php
+++ app/Repositories/PlanetDistanceRepository/FromConfig/PlanetDistanceFromConfigRepository.php
@@ -3,16 +3,16 @@
 namespace App\Repositories\PlanetDistanceRepository\FromConfig;
 
 use App\DTOs\PlanetDistanceDto;
+use App\Exceptions\LostAPlanetMasterObiWanHasException;
 use App\Repositories\PlanetDistanceRepository\PlanetDistanceRepositoryInterface;
 
 class PlanetDistanceFromConfigRepository implements PlanetDistanceRepositoryInterface
 {
     public function calculate(string $from, string $to): PlanetDistanceDto
     {
-        /*
-         * ToDo: Convert shit here
-         */
-
+        $distances = config("planets.$from") ?? config("planets.$to") ?? null;
+        $distance = $distances[$to] ?? $distances[$from] ?? null;
+        throw_if(!$distance, new LostAPlanetMasterObiWanHasException("How Embarrassing"));
 
         return new PlanetDistanceDto([]);
     }
