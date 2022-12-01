8d88b5ba54b8fb050b0db394a82f7f0fd487dc04
rob.burley <rob.burley@itech.media>
2022-12-01 11:15:26 +0000

    refactor

--- app/Repositories/PlanetDistanceRepository/FromConfig/PlanetDistanceFromConfigRepository.php
+++ app/Repositories/PlanetDistanceRepository/FromConfig/PlanetDistanceFromConfigRepository.php
@@ -13,14 +13,12 @@ class PlanetDistanceFromConfigRepository implements PlanetDistanceRepositoryInte
         $from = strtolower($from);
         $to = strtolower($to);
 
-        if ($from == $to) {
-            $distance = 0;
-        } else {
-            $distance = config("planets.$from.$to") ?? config("planets.$to.$from") ?? null;
+        $distance = $from !== $to
+            ? config("planets.$from.$to") ?? config("planets.$to.$from") ?? null
+            : 0;
 
-            throw_if(!$distance, new LostAPlanetMasterObiWanHasException("How Embarrassing"));
-        }
-        
+        throw_if(is_null($distance), new LostAPlanetMasterObiWanHasException("How Embarrassing"));
+
         return new PlanetDistanceDto($from, $to, $distance);
     }
 }
